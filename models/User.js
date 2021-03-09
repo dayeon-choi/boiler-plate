98a7001dbbdafb844632fb69020806ac8eabe9b2
dayeon <dayeon2399@naver.com>
2021-03-10 00:44:27 +0900

    dayeon's commit - encryption

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+const saltRounds=10
 
 const userSchema=mongoose.Schema({
     name:{
@@ -28,6 +30,24 @@ const userSchema=mongoose.Schema({
     },
     tokenExp:{
         type:Number
+    }
+})
+
+userSchema.pre('save',function(next){
+    var user=this;
+    
+    //필드 중 password가 변화될 때만 dcrypt를 이용하여 암호화
+    if(user.isModified('password')){    
+        //비밀번호를 암호화 시킴
+        bcrypt.genSalt(saltRounds,function(err,salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err)
+                user.password = hash    //hash된 비밀번호로 바꾸어줌
+                next()  //돌아감
+            })
+        })
     }
 })
 

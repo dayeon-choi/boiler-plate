8c553b0f771fead6a1ca503669a47849f09c15c9
dayeon <dayeon@naver.com>
2021-03-09 23:11:58 +0900

    dayeon's commit - joining

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-from-urlencoded -> 이런 데이터를 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.urlencoded({extended:true}));  
+//application/json -> json타입으로 된 것을 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://dayeonchoi:abc123123@boilerplate.adl9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -10,7 +17,22 @@ mongoose.connect('mongodb+srv://dayeonchoi:abc123123@boilerplate.adl9t.mongodb.n
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요~~')
+  res.send('Hello World! 새해 복 많이 받으세요!')
+})
+
+app.post('/register',(req,res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어줌
+
+  const user = new User(req.body)
+
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success: false,err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
 app.listen(port, () => {
